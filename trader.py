9a05a13488e7dacda3d897d5f8c9753a4dca4142
rde31 <coder2@supplycart.my>
2020-09-04 16:35:08 +0800

    Saving changes

--- trader.py
+++ trader.py
@@ -1,4 +1,6 @@
 from binance.client import Client
+import time
+
 client = Client('uuMkEps4tMUnkj0IJpDkzJRilyzylb0ajLpvQC1a9aad5X9hKSOcNLcRkXbNPcKE', 'VBmJ8JBeLglFIO3eT83lwKsAdI0PowjUf95EAlUoKiKbMf9aIQW6eO0CexXEq6su')
 client.API_URL = 'https://testnet.binance.vision/api' 
 # testnet url
@@ -9,24 +11,27 @@ from twisted.internet import reactor
 def readPrice(msg):
     ''' define how to process incoming WebSocket messages '''
     if msg['e'] != 'error':
-        print(msg['s'])
         price['last'] = msg['c']
-        if entry < price['last']:
+        if entry < float(price['last']):
             print('buy')
-            bsm.stop_socket(conn_key)
-            reactor.stop()
             trade['pair'] = tradingPair
             trade['entered'] = True
-            
+            tradeActive(trade) 
     else:
         price['error'] = True
+
+def tradeActive(t):
+    while trade['entered']:
+        time.sleep(5)
+        print('trade active')
+    return
 
 price = {'error' : False} 
 trade = {'pair' : None, 'entered' : False}
 bsm = BinanceSocketManager(client)
-tradingPair = input('Enter trading pair: ')
-entry = input('Enter entry price (in left side of pairing): ')
-conn_key = bsm.start_symbol_ticker_socket(tradingPair, readPrice)
+tradingPair = 'ETHUSDT' #test
+entry = 365 #test
+conn_key = bsm.start_symbol_miniticker_socket(tradingPair, readPrice)
 bsm.start()
 
 
