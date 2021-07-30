8d15987df703a81142d9e465c6179317c8590b64
RDEvans31 <robert.d.evans3100@gmail.com>
2021-07-30 12:51:50 +0800

    Fix for profit taking in swing trader

--- ETHUSD_mean_reversion.py
+++ ETHUSD_mean_reversion.py
@@ -24,8 +24,11 @@ bb_period=8
 multiple=1.3
 atr_period=16
 
-def get_balance():
+def get_free_balance():
     return float(next(filter(lambda x:x['coin']=='USD', MeanReversion.get_balances()))['free'])
+
+def get_total_balance():
+  return float(next(filter(lambda x:x['coin']=='USD', MeanReversion.get_balances()))['total'])
 
 def append_new_line(file_name, text_to_append):
     """Append given text as a new line at the end of file"""
@@ -56,7 +59,7 @@ if position_size==0:
     daily=price.get_price_data('1d',symbol='ETH/USD')
     hourly=price.get_price_data('1h',symbol='ETH/USD')
     state=chart.identify_trend(daily,hourly)
-    trade_capital=get_balance()*1.5
+    trade_capital=get_free_balance()*1.5
     position_size=round(trade_capital/hourly.iloc[-1]['close'],precision)
 
 elif position['side']=='buy':
@@ -86,6 +89,8 @@ def run():
     active_trade=position_size!=0
 
     if active_trade:
+      print('Active trade. ')
+      print('Bollinger bands: %s, open price: %s' % ((str(upper_limit)+', '+str(lower_limit)), current_price))
       PnL=float(position['recentPnl'])
       entry=float(position['recentBreakEvenPrice'])
       #check for conditions to close trade
@@ -97,16 +102,16 @@ def run():
         active_trade=False
       else:
         print('Trade still active')
-
     else:
-      position_size=0.01#round((get_balance()*1.5)/current_price,precision)
+      
+      position_size=0.01#round((get_free_balance()*1.5)/current_price,precision)
       if current_gradient>0 and current_price<lower_limit:
           output_string='long @ '+ str(current_price)+' :'+datetime.utcnow().strftime("%m/%d/%y, %H:%M,%S")
           print('long @ '+datetime.utcnow().strftime("%m/%d/%y, %H:%M,%S"))
           ftx_ccxt.create_order('ETH-PERP','market','buy',position_size)
           #ftx_ccxt.create_limit_buy_order('ETH-PERP',position_size,current_price)
           sl=current_price-current_atr
-          trigger=current_price-0.95*current_atr
+          trigger=current_price-0.99*current_atr
           risk=abs(sl/current_price -1 )
           if (risk<=0.03):
             MeanReversion.place_conditional_order('ETH-PERP','sell',position_size,'stop',limit_price=sl,trigger_price=trigger)
@@ -116,7 +121,7 @@ def run():
           print('short @ '+datetime.utcnow().strftime("%m/%d/%y, %H:%M,%S"))
           ftx_ccxt.create_order('ETH-PERP','market','sell',position_size)
           sl=current_price+current_atr
-          trigger=current_price+0.95*current_atr
+          trigger=current_price+0.99*current_atr
           risk=abs(sl/current_price -1 )
           if (risk<=0.03):
             MeanReversion.place_conditional_order('ETH-PERP','buy',position_size,'stop',limit_price=sl,trigger_price=trigger)
@@ -127,7 +132,8 @@ def run():
           append_new_line('ETH_meanReversion_log.txt',output_string)
           print(output_string)
       else:
-        print(output_string)
+        print('no change')
+
 
     time_till_next_hour=3600-time.time()%3600
     time.sleep(time_till_next_hour-5)
