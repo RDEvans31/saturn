3a3b80b83b36bc37abf5b1926fbfe68a1ef39f5b
RDEvans31 <robert.d.evans3100@gmail.com>
2021-11-09 10:25:49 +0000

    adding functionality for new users

--- accumulator.py
+++ accumulator.py
@@ -1,13 +1,11 @@
 import ccxt
+from pandas.core import api
 import price_data
 import chart
-import time
-import schedule
 import numpy as np
-from datetime import datetime
-from ftx_client import FtxClient
 from gql import gql, Client
 from gql.transport.aiohttp import AIOHTTPTransport
+from users import users
 
 # Select your transport with a defined url endpoint
 transport = AIOHTTPTransport(url="https://saturn.hasura.app/v1/graphql", headers={'x-hasura-admin-secret': 'Rc07SJt4ryC6RyNXDKFRAtFmRkGBbT8Ez3SdaEYsHQoHemCldvs52Kc803oK8X62'})
@@ -28,22 +26,29 @@ cex= ccxt.cex({
     'enableRateLimit': True,
     })
 
-ftx.headers = {'FTX-SUBACCOUNT':'Savings'}
-
-def ftx_get_free_balance():
-    return float(next(filter(lambda x:x['coin']=='USD', Savings.get_balances()))['free'])
-
 def get_limit(symbol, markets):
     return float(next(filter(lambda x:x['symbol']==symbol, markets))['limits']['amount']['min'])
 
 
-main=FtxClient(api_key='mFRyLR4AAhLTc5RlWov3PKTcIbMHw3vGZwiHnsrn',api_secret='oKaY1WEqTuhnNnq0iRi_Ry-CYckvE89-gPUPf21B')
-Savings=FtxClient(api_key='mFRyLR4AAhLTc5RlWov3PKTcIbMHw3vGZwiHnsrn',api_secret='oKaY1WEqTuhnNnq0iRi_Ry-CYckvE89-gPUPf21B',subaccount_name='Savings')
-MeanReversion=FtxClient(api_key='mFRyLR4AAhLTc5RlWov3PKTcIbMHw3vGZwiHnsrn',api_secret='oKaY1WEqTuhnNnq0iRi_Ry-CYckvE89-gPUPf21B',subaccount_name='MeanReversion')
+
 
 #1. read from list of dictonaries conatining investments.
 #2. for each investment, purchase amount equivalent to money set aside
 #3. update mean entry, and set stoposs at 5% from entry 
+
+for key, user in users.items():
+    daily_buy_amount=user['daily_buy_amount']
+    for name, details in user['exchanges'].items():
+        exchange: ccxt.Exchange
+        if name=='ftx':
+            exchange = ccxt.ftx(details['api'])
+        elif name=='cex':
+            exchange = ccxt.ftx(details['api'])
+        else:
+            exchange = ccxt.ftx(details['api'])
+        symbols=details['symbols']
+
+
 markets=ftx.fetch_markets()
 
 daily_buy_amount=10
@@ -57,83 +62,11 @@ risk=chart.risk_indicator(fast_ema,slow).iloc[-1]['value'].item()
 print('BTC risk level: ', risk)
 price_data.update_database('ETH/USD','1d')
 price_data.update_database('ETH/BTC','1d')
-if risk>=0.5:
-    ethbtc=price_data.get_stored_data('ETH/BTC','1d')
-    ethbtc_weekly=price_data.get_price_data('1w', data=ethbtc) 
-    slow_ethbtc=chart.get_sma(ethbtc_weekly,15)
-    risk_ethbtc=chart.risk_indicator(ethbtc,slow_ethbtc).iloc[-1]['value'].item()
-    print('ETHBTC risk level', risk_ethbtc)
-    #sell btc
-    price=price_data.get_price_data('1m', symbol='BTC/USD')
-    try:
-        balance=float(ftx.fetch_partial_balance('BTC')['total'])
-    except:
-        print('No balance for BTC ')
-    current_price=price.iloc[-1]['close'].item()
-    dynamic_sell_amount=round((-1/(50*(risk-1))), 2)*balance
-    Savings.place_order('BTC/USD','sell',price=current_price, type='limit', size=dynamic_sell_amount)
-    print('Sold %s of %s' %(dynamic_sell_amount, 'BTC/USD'))
-    usd_amount=dynamic_sell_amount*current_price
-    if risk_ethbtc<0.5: #move those profits over to eth
-            usd_balance=ftx.fetch_partial_balance('USD')['total']   
-            eth_price=price_data.get_price_data('1m',symbol='ETH/USD')       
-            #buy
-            current_eth_price=eth_price.iloc[-1]['close'].item()
-            buy_amount=usd_amount/current_eth_price
-            if buy_amount<usd_balance:
-                amount_to_buy=round(buy_amount/current_eth_price,3) #eth has a precision of 0.001
-                limit=get_limit('ETH/USD', markets)
-                if amount_to_buy>limit:
-                    Savings.place_order('ETH/USD','buy',price=current_price, type='limit', size=amount_to_buy)
-                    print('Bought %s of %s' %(amount_to_buy, 'ETH/USD'))
-                else:
-                    print('Amount not above limit: %s, %s' % (limit,amount_to_buy))
-                    Savings.place_order('ETH/USD','buy',price=current_price, type='limit', size=limit)
-                    print('Bought %s of %s' %(limit, 'ETH/USD'))
+ethbtc=price_data.get_stored_data('ETH/BTC','1d')
+ethbtc_weekly=price_data.get_price_data('1w', data=ethbtc) 
+slow_ethbtc=chart.get_sma(ethbtc_weekly,15)
+risk_ethbtc=chart.risk_indicator(ethbtc,slow_ethbtc).iloc[-1]['value'].item()
 
-    else:#start selling alts
-        #selling
-        for i in range(len(symbols)-1): #sells everything except btc
-            symbol=symbols[i+1]
-            price=price_data.get_price_data('1m', symbol=symbol)
-            noDecimals=np.absolute(np.log10(next(filter(lambda x:x['symbol']==symbol, ftx.fetch_markets()))['precision']['amount']))
-            try:
-                balance=float(ftx.fetch_partial_balance(symbol.split('/')[0])['total'])
-            except:
-                print('No balance for : ', symbol)
-            current_price=price.iloc[-1]['close'].item()
-            dynamic_sell_amount=round(round((-1/(50*(risk_ethbtc-1))), 2)*balance,noDecimals)
-            Savings.place_order(symbol,'sell',price=current_price, type='limit', size=dynamic_sell_amount)
-            print('Sold %s of %s' %(dynamic_sell_amount, symbol))
-
-
-
-elif risk<0.5:
-    for i in range(len(symbols)):
-        usd_balance=ftx.fetch_partial_balance('USD')['total']
-        symbol=symbols[i]
-        price=price_data.get_price_data('1m', symbol=symbol)
-        noDecimals=np.absolute(np.log10(next(filter(lambda x:x['symbol']==symbol, ftx.fetch_markets()))['precision']['amount']))
-        try:
-            balance=float(ftx.fetch_partial_balance(symbol.split('/')[0])['total'])
-        except:
-            print('No balance for : ', symbol)
-        current_price=price.iloc[-1]['close'].item()
-            
-            #buy
-        dynamic_buy_amount=round(daily_buy_amount*((0.5/(risk-0.15))-1),2)
-        if dynamic_buy_amount<usd_balance:
-            amount_to_buy=round(dynamic_buy_amount/current_price,4)
-            limit=get_limit(symbol, markets)
-            if amount_to_buy>limit:
-                Savings.place_order(symbol,'buy',price=current_price, type='limit', size=amount_to_buy)
-                print('Bought %s of %s' %(amount_to_buy, symbol))
-            else:
-                print('Amount not above limit: %s, %s' % (limit,amount_to_buy))
-                Savings.place_order(symbol,'buy',price=current_price, type='limit', size=limit)
-                print('Bought %s of %s' %(limit, symbol))
-        else:
-            print('No balance')
 
 
 
---
+++ users.py
@@ -0,0 +1,24 @@
+users = {
+    'rob': {
+        'daily_buy_amount': 10,
+        'exchanges': {
+            'ftx': {
+                'symbols':['BTC/USD', 'ETH/USD', 'SOL/USD', 'MATIC/USD'],
+                'api':{
+                    'apiKey': 'mFRyLR4AAhLTc5RlWov3PKTcIbMHw3vGZwiHnsrn',
+                    'secret': 'oKaY1WEqTuhnNnq0iRi_Ry-CYckvE89-gPUPf21B',
+                    'enableRateLimit': True,
+                }
+            },
+            'cex':{
+                'symbols':['ADA/USD'],
+                'api':{
+                    'uid' : 'up109520414',
+                    'apiKey': '1X2uEcPlvBCe4CcMtzWKguG1SDI',
+                    'secret': '8JY4fDg6hRz0DTolZHz77XPdC1o',
+                    'enableRateLimit': True,
+                }
+            }
+        }
+    }
+}
